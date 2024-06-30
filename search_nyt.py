8eb8e1963a278a5c216a5dacf23678205191fa85
yuchienchen <yuchienc@yahoo.com>
2024-06-29 22:49:14 -0500

    update defs

--- search_nyt.py
+++ search_nyt.py
@@ -1,15 +1,21 @@
-
+import requests
 
 API_KEY = "ocPGtmzAk2JgzyEmO0dZHJxDUIJcBhTo"
 API_BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 
 def search_articles(search_term):
-    pass
+    params = {
+        "q": search_term, 
+        "api-key": API_KEY
+    }
+    response = requests.get(API_BASE_URL, params)
+    return response.json()
 
 def display_results(search_results):
-    pass
+    print(search_results)
 
 while True:
     search_term = input("Your search term: ")
     search_results = search_articles(search_term)
     display_results(search_results)
+    print("")
